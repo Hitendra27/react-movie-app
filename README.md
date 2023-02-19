af4c0f30d52d265c9946d9147fb3845b25c3e9c1
Hitendra Quessou <sanjay_quessou@yahoo.co.uk>
2023-02-19 03:36:25 +0000

    Local Stroage for favourites.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# React Movie app 
+
+A movie app built with React.js 
+
+### screenshots of the app.
+
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,3 +78,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
--- src/App.js
+++ src/App.js
@@ -9,55 +9,73 @@ import RemoveFavourites from './components/RemoveFavourites';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [favourties, setFavourties] = useState([]);
-  const [searchValue, setSearchValue] = useState('')
+  const [favourites, setFavourites] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async (searchValue) => {
+  const getMovieRequest = async searchValue => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=803bd1f2`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
 
-    if(responseJson.Search) {
+    if (responseJson.Search) {
       setMovies(responseJson.Search);
     }
-   
   };
 
   useEffect(() => {
-    getMovieRequest(searchValue)
+    getMovieRequest(searchValue);
   }, [searchValue]);
 
-  const addFavourtineMovie = (movie) => {
-    const newFavourteList = [...favourties, movie];
-    setFavourties(newFavourteList);
+  useEffect(() => {
+    const movieFavourites = JSON.parse(localStorage.getItem('react-movie-app-favourites')
+    );
+    setFavourites(movieFavourites);
+  }, []);
 
-  }
+  const saveToLocalStorage = items => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+  };
+
+  const addFavouriteMovie = movie => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
+
+  const removeFavouriteMovie = movie => {
+    const newFavouriteList = favourites.filter(
+      favourite => favourite.imdbID != movie.imdbID
+    );
+
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
   return (
     <div className="container-fluid movie-app">
-      <div className='row d-flex align-items-center mt-4 mb-4'>
+      <div className="row d-flex align-items-center mt-4 mb-4">
         <MovieListHeading heading="Movies" />
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
-        <MovieList 
-        movies={movies} 
-        handleFavouritesClick={addFavourtineMovie} 
-        favouriteComponent={AddFavourities}
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddFavourities}
         />
       </div>
-      <div className='row d-flex align-items-center mt-4 mb-4'>
+      <div className="row d-flex align-items-center mt-4 mb-4">
         <MovieListHeading heading="Favourites" />
-        </div>
-        <div className="row">
-        <MovieList 
-        movies={favourties} 
-        handleFavouritesClick={addFavourtineMovie} 
-        favouriteComponent={RemoveFavourites}
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouriteMovie}
+          favouriteComponent={RemoveFavourites}
         />
       </div>
     </div>
   );
-};
+}
 
 export default App;
